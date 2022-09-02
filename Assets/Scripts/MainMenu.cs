53699c0bd69666dffec57b2893645bce9f15a107
Tyler Funk <tcfunk25@gmail.com>
2022-09-02 11:59:00 -0500

    options button displays options menu, back button goes back to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,24 +8,39 @@ using UnityEngine.UIElements;
 public class MainMenu : MonoBehaviour
 {
     private VisualElement ui;
-    
+
+    private VisualElement mainMenu;
+    private VisualElement optionsMenu;
+
     private void Start()
     {
         ui = GetComponent<UIDocument>().rootVisualElement;
+
+        mainMenu = ui.Q<VisualElement>("menu_main");
+        optionsMenu = ui.Q<VisualElement>("menu_options");
         
+        // Set default state.
+        optionsMenu.style.display = DisplayStyle.None;
+
         ui.Q<Button>("button_start").RegisterCallback<ClickEvent>(e =>
         {
             Debug.Log("clicked button_start");
             SceneManager.LoadScene("Scenes/Playground");
         });
-        
+
         ui.Q<Button>("button_options").RegisterCallback<ClickEvent>(e =>
         {
-            Debug.Log("options");
+            mainMenu.style.display = DisplayStyle.None;
+            optionsMenu.style.display = DisplayStyle.Flex;
+            // optionsMenu.enabledSelf = false;
         });
         
-        ui.Q<Button>("button_quit").RegisterCallback<ClickEvent>(e =>
-        {
+        ui.Q<Button>("button_close_options").RegisterCallback<ClickEvent>(e =>  {
+            optionsMenu.style.display = DisplayStyle.None;
+            mainMenu.style.display = DisplayStyle.Flex;
+        });
+
+        ui.Q<Button>("button_quit").RegisterCallback<ClickEvent>(e => {
             Application.Quit();
             Debug.Log("Quit");
         });
